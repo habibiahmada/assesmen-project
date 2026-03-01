078b208dca37b68d4d6890f4c53673c815454397
habibiahmada <habibiahmadaziz@gmail.com>
2026-03-01 13:45:08 +0700

    feat: add connection and model

--- multi-role/second-try/models/User.php
+++ multi-role/second-try/models/User.php
@@ -1,32 +1,40 @@
 <?php
-class User {
+class User{
     private $conn;
 
-    public function __construct($conn) {
+    public function __construct($conn)
+    {
         $this->conn = $conn;
     }
 
-    public function findByUsername($username) {
-        $sql = "SELECT * FROM users WHERE username = ? LIMIT 1";
+    public function findUserByName($username) {
+        $sql = 'SELECT * FROM users WHERE username = ? LIMIT 1';
+
         $stmt = mysqli_prepare($this->conn, $sql);
+
         mysqli_stmt_bind_param($stmt, 's', $username);
         mysqli_stmt_execute($stmt);
+
         $result = mysqli_stmt_get_result($stmt);
+
         $user = mysqli_fetch_assoc($result);
+
         mysqli_stmt_close($stmt);
+
         return $user ?: null;
+
     }
 
     public function createUser($username, $passwordHash, $role = 'peminjam') {
-        $sql = "INSERT INTO users (username, password, role) VALUES (?, ?, ?)";
+        $sql = 'INSERT INTO users (username, password, role) VALUES (?, ?, ?)';
+
         $stmt = mysqli_prepare($this->conn, $sql);
 
         mysqli_stmt_bind_param($stmt, 'sss', $username, $passwordHash, $role);
         $ok = mysqli_stmt_execute($stmt);
-        
+
         mysqli_stmt_close($stmt);
+
         return $ok;
     }
 }
-
-?>
